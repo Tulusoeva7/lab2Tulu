3d7069b505cbcf7999b1da8bba78e93e538e24db
Tulusoeva7 <146403993+Tulusoeva7@users.noreply.github.com>
2025-04-13 19:50:27 +0300

    Update Projectte.cpp

--- Projectte/Projectte.cpp
+++ Projectte/Projectte.cpp
@@ -1,62 +1,103 @@
-﻿#include <glew.h>
+#include <glew.h>
 #include <glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <cmath>
 
-const char* vertexShaderSource = R"(
-#version 410 core
-layout (location = 0) in vec3 aPos;
-void main() {
-    gl_Position = vec4(aPos, 1.0);
-})";
+void error_callback(int error, const char* description) {
+    std::cerr << "GLFW Error: " << description << std::endl;
+}
 
-const char* fragmentShaderSource = R"(
-#version 410 core
-out vec4 FragColor;
-uniform float timeValue;
-void main() {
-    float greenValue = (sin(timeValue) / 2.0) + 0.5;
-    FragColor = vec4(0.0, greenValue, 1.0, 1.0);
-})";
+std::string readShaderFile(const char* path) {
+    std::ifstream shaderFile(path);
+    if (!shaderFile) {
+        std::cerr << "Failed to open shader file: " << path << std::endl;
+        return "";
+    }
 
-float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.5f,  0.5f, 0.0f,
-    -0.5f,  0.5f, 0.0f
-};
+    std::stringstream shaderStream;
+    shaderStream << shaderFile.rdbuf();
+    return shaderStream.str();
+}
 
-unsigned int indices[] = {
-    0, 1, 2,
-    2, 3, 0
-};
+GLuint compileShader(const std::string& source, GLenum type) {
+    GLuint shader = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(shader, 1, &src, nullptr);
+    glCompileShader(shader);
 
-void checkShaderCompilation(GLuint shader) {
     GLint success;
-    GLchar infoLog[512];
     glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
     if (!success) {
-        glGetShaderInfoLog(shader, 512, NULL, infoLog);
-        std::cerr << "Shader Compilation Error:\n" << infoLog << std::endl;
+        char infoLog[1024];
+        glGetShaderInfoLog(shader, 1024, nullptr, infoLog);
+        std::cerr << "Shader compile error:\n" << infoLog << std::endl;
     }
+
+    return shader;
+}
+
+GLuint createShaderProgram(const std::string& vertexPath, const std::string& fragmentPath) {
+    std::string vertexCode = readShaderFile(vertexPath.c_str());
+    std::string fragmentCode = readShaderFile(fragmentPath.c_str());
+
+    GLuint vertexShader = compileShader(vertexCode, GL_VERTEX_SHADER);
+    GLuint fragmentShader = compileShader(fragmentCode, GL_FRAGMENT_SHADER);
+
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+
+    GLint success;
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success) {
+        char infoLog[1024];
+        glGetProgramInfoLog(program, 1024, nullptr, infoLog);
+        std::cerr << "Shader link error:\n" << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return program;
 }
 
 int main() {
     if (!glfwInit()) {
-        std::cerr << "Failed to initialize GLFW" << std::endl;
+        std::cerr << "Failed to init GLFW\n";
         return -1;
     }
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "VBO, VAO, EBO Example", NULL, NULL);
+    glfwSetErrorCallback(error_callback);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Pink Square from Files", nullptr, nullptr);
     if (!window) {
-        std::cerr << "Failed to create GLFW window" << std::endl;
+        std::cerr << "Window creation failed\n";
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
     glewInit();
 
-    GLuint VBO, VAO, EBO;
+    float vertices[] = {
+        -0.25f, -0.25f, 0.0f,
+         0.25f, -0.25f, 0.0f,
+         0.25f,  0.25f, 0.0f,
+        -0.25f,  0.25f, 0.0f
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        0, 2, 3
+    };
+
+    GLuint VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
@@ -69,46 +110,37 @@ int main() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
     glEnableVertexAttribArray(0);
 
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    checkShaderCompilation(vertexShader);
-
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    checkShaderCompilation(fragmentShader);
-
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glUseProgram(shaderProgram);
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    GLuint shaderProgram = createShaderProgram("vertex.glsl", "fragment.glsl");
 
     while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.05f, 0.05f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
-        float timeValue = glfwGetTime();
-        int timeLocation = glGetUniformLocation(shaderProgram, "timeValue");
-        glUniform1f(timeLocation, timeValue);
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
+
+        float time = glfwGetTime();
+        float r = 1.0f;
+        float g = 0.2f + 0.2f * sin(time);
+        float b = 1.0f;
+
+        GLint colorLoc = glGetUniformLocation(shaderProgram, "ourColor");
+        glUniform4f(colorLoc, r, g, b, 1.0f);
+
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glDeleteVertexArrays(1, &VAO);
+    glDeleteProgram(shaderProgram);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
+    glDeleteVertexArrays(1, &VAO);
+
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
